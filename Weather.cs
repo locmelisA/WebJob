f246a2d5bab8698af1fbe9dd12b125d9ac51f202
locmelisA <57525563+locmelisA@users.noreply.github.com>
2020-02-28 12:03:45 +0100

    Add files via upload

---
+++ Weather.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebJob
+{
+    public class Weather
+    {
+        public double Temp;
+        public double Pressure;
+        public double Humidity;
+    }
+}
